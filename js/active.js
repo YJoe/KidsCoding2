b7aaa4b5282ea7f99eca3049a704ca45c9f30463
Kikadass <kikadass@gmail.com>
2016-03-08 20:30:29 +0000

    AUTOMATED NAVBAR

--- js/active.js
+++ js/active.js
@@ -1,5 +1,23 @@
-$('#home').load('Home.html');
-$('#aboutus').load('AboutUs.html');
-$('#donate').load('Donate.html');
-$('#volunteer').load('Volunteer.html');
-$('#contact').load('Contact.html');
+$(function() {
+    var mynavbar = $("#navbar"); //cache it
+    $.get("Navbar.html")
+        .done(function( data ) {
+            mynavbar.html(data);
+            // remove any current active if found in markup:
+            mynavbar.find('ul.nav li').removeClass('active');
+
+            var url = window.location;// get location
+
+            // add the active class to current url detected href
+            // Will work for absolute hrefs, might have to adjust for others
+            mynavbar.find('ul.nav li').find('a').filter(function() {
+                return (url.indexOf(this.href) != -1);
+            }).parent().addClass('active');
+        });
+    // put the click handler for the navigation in place
+    mynavbar.on('click', 'ul.nav li', function() {
+        var myhref = $(this).find('a').attr('href');
+        alert("proceed to: " + myhref);
+        window.location = myhref;
+    });
+});
