45a3cc46c705d18f64bffb4cb1db7b798fded8a3
Kikadass <kikadass@gmail.com>
2016-03-13 13:35:55 +0000

    Navbar finished and fixed

--- js/active.js
+++ js/active.js
@@ -3,17 +3,21 @@ $(function() {
     $.get("Navbar.html")
         .done(function( data ) {
             mynavbar.html(data);
+
             // remove any current active if found in markup:
             mynavbar.find('ul.nav li').removeClass('active');
 
-            var url = window.location;// get location
+            var url = window.location.href;// get location
 
             // add the active class to current url detected href
             // Will work for absolute hrefs, might have to adjust for others
             mynavbar.find('ul.nav li').find('a').filter(function() {
                 return (url.indexOf(this.href) != -1);
             }).parent().addClass('active');
+
+
         });
+
     // put the click handler for the navigation in place
     mynavbar.on('click', 'ul.nav li', function() {
         var myhref = $(this).find('a').attr('href');
