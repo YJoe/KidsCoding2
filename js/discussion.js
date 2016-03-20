780295e0834d8a8fc74fabd405afa2c2e4f5a68c
YJoe <YJoe762@gmail.com>
2016-03-20 00:43:31 +0000

    Merge branch 'master' of C:\Users\Joe\WebstormProjects\KidsCoding2 with conflicts.

--- js/discussion.js
+++ js/discussion.js
@@ -2,26 +2,38 @@ var threads = 0;
 
 function addNewThread() {
     $("#discussionThread").append($(
-        "<div class = 'messageBox' >" +
-        "<div class = 'bodyText' "+
-        " style='color: #fbfbfb'>" +
-        $('#messageInput').val() +
+        "<div class = 'messageBox'>" +
+            "<div class = 'bodyText'>" +
+                "<div class = subMessageBox>" +
+                    $('#messageInput').val() +
+                "</div>" +
+            "</div>" +
+            "<div id = 'thread" + threads + "'>"+
         "</div>" +
-        "<div id = 'thread" + threads + "'>"+
-        "</div>" +
-        "<input type='text' id = 'subMessageInput" + threads + "' class='form-control' name='messageInput' required placeholder='Send a message'>" +
-        "<button class='btn btn-primary' onclick='messageInThread(" + threads + ")'>Reply</button>"
+        "<div class = input-group>" +
+            "<input type='text' id = 'subMessageInput" + threads + "' class='form-control' style='margin: 20px; margin-top: 0;' placeholder='Reply to this thread...'>"+
+            "<span class='input-group-btn'>" +
+                "<button class='btn btn-default' type='button' style='margin: 20px; margin-top: 0' onclick='messageInThread(" + threads + ")'>Send</button>"+
+            "</span>" +
+        "</div>"
     ));
     threads += 1;
 }
 
 function messageInThread(threadID){
+    var d = new Date();
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+    var date = "Date " + d.getFullYear() + '/' + ((''+month).length<2 ? '0' : '') + month + '/' + ((''+day).length<2 ? '0' : '') + day;
+    date += " Time " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() ;
     var threadDiv = '#' + 'thread' + threadID;
     var threadIn = '#' + 'subMessageInput' + threadID;
 
     $(threadDiv).append(
         "<div class = 'subMessageBox'>" +
-        $(threadIn).val()+
+            "<div class = bodyText>" +
+                $(threadIn).val() +
+            "</div>" +
         "</div>"
     )
 }
