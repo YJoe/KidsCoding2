60a549c231870c63d9086475a6dba5a12d0bb4be
YJoe <YJoe762@gmail.com>
2016-03-22 02:01:05 +0000

    Discussion board improvements, donation and volunteer form corrections

--- js/discussion.js
+++ js/discussion.js
@@ -5,31 +5,43 @@ function validateForm(formId) {
     var form=document.getElementById(formId);
     inputs = form.getElementsByTagName('input');
     for (index = 0; index < inputs.length; ++index) {
-        // deal with inputs[index] element.
         if (inputs[index].value==null || inputs[index].value=="") {
-            //alert("Field is empty");
+            alert("Field is empty");
             return false;
         }
     }
     return true;
 }
 
+function getDate(){
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var date = "Date " + d.getFullYear() + '/' + (('' + month).length < 2 ? '0' : '') + month + '/' + (('' + day).length < 2 ? '0' : '') + day;
+    date += " Time " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+    return date;
+}
+
 function addNewThread(validation) {
     if (validation) {
         $("#discussionThread").append($(
-            "<div class = 'messageBox'>" +
-            "<div class = 'bodyText'>" +
-            "<div class = subMessageBox>" +
-            $('#messageInput').val() +
-            "</div>" +
-            "</div>" +
+            "<div class = 'box' style = 'background-color: #65bfcc; margin-top: 30px'>" +
+                "<div class = subMessageBox style = 'padding: 20px;'>" +
+                    "<div class = 'bodyText'>" + "" +
+                        $('#messageInput').val() +
+                        "<div style='font-family: Roboto-Bold'>" +
+                        "[" + getDate() + "]" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
             "<div id = 'thread" + threads + "'>" +
             "</div>" +
             "<div class = input-group id='form" + threads +"'>" +
-            "<input type='text' id = 'subMessageInput" + threads + "' class='form-control' style='margin: 20px; margin-top: 0;' placeholder='Reply to this thread...'>" +
-            "<span class='input-group-btn'>" +
-            "<button class='btn btn-default' type='button' style='margin: 20px; margin-top: 0' onclick='messageInThread(" + threads + ", validateForm(\"form" + threads + "\"))'>Send</button>" +
-            "</span>" +
+                "<input type='text' id = 'subMessageInput" + threads + "' class='form-control' style='margin: 20px; margin-top: 0;' placeholder='Reply to this thread...'>" +
+                "<span class='input-group-btn'>" +
+                    "<button class='btn btn-default' type='button' style='margin: 20px; margin-top: 0' onclick='messageInThread(" + threads + ", validateForm(\"form" + threads + "\"))'>Send</button>" +
+                "</span>" +
             "</div>"
         ));
 
@@ -39,18 +51,16 @@ function addNewThread(validation) {
 
 function messageInThread(threadID, validation){
     if (validation) {
-        var d = new Date();
-        var month = d.getMonth() + 1;
-        var day = d.getDate();
-        var date = "Date " + d.getFullYear() + '/' + (('' + month).length < 2 ? '0' : '') + month + '/' + (('' + day).length < 2 ? '0' : '') + day;
-        date += " Time " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
         var threadDiv = '#' + 'thread' + threadID;
         var threadIn = '#' + 'subMessageInput' + threadID;
 
         $(threadDiv).append(
-            "<div class = 'subMessageBox'>" +
+            "<div class = 'subMessageBox' style='padding: 20px'>" +
             "<div class = bodyText>" +
             $(threadIn).val() +
+            "<div style='font-family: Roboto-Bold'>" +
+            " [" + getDate() + "]" +
+            "</div>" +
             "</div>" +
             "</div>"
         )
